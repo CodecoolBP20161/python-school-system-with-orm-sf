13a104304ee3f1d69237221cb881506865074c39
Adam <horada1991@gmail.com>
2016-07-20 10:18:43 +0200

    Necessary models implemeted

--- models.py
+++ models.py
@@ -3,10 +3,28 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('dbname', user='dbuser')
+db = PostgresqlDatabase('cave', user='cave', password='123456789')
 
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
     class Meta:
         database = db
+
+
+class School(BaseModel):
+    location = CharField()
+
+
+class Applicant(BaseModel):
+    application_code = CharField(default='None')
+    first_name = CharField()
+    last_name = CharField()
+    city = CharField()
+    school = ForeignKeyField(School, default='None', related_name='applicants')
+
+
+class City(BaseModel):
+    name = CharField()
+    school_near = ForeignKeyField(School, related_name='schools')
+
