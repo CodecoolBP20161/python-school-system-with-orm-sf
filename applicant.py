6f89061946dc60c49c5f8948cfe85de12d251d56
balag3 <carbalage@gmail.com>
2016-08-04 11:13:00 +0200

    Adds applicant ligin class

---
+++ applicant.py
@@ -0,0 +1,14 @@
+from models import *
+
+class ApplicantLogin():
+
+    def __init__(self,  appcode):
+        self.appcode = appcode
+
+    @classmethod
+    def login(cls):
+        passwd = input("Please add a passcode")
+        if Applicant.get(application_code == passwd):
+            return ApplicantLogin(appcode)
+        else:
+            print("No such applicant")
