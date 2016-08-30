e8ba958a84ec24266510cb50b0e97460fb15a3ac
Ádám Horváth <horada1991@gmail.com>
2016-08-30 12:02:33 +0200

    hotfix
    submit_applicant fixed

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 class MyForm(Form):
-    first_name = StringField('Firs Name')
+    first_name = StringField('First Name')
     last_name = StringField('Last Name')
     city = StringField('City')
     email = StringField('Email', validators=[validators.Email(message='Invalid email address!')])
@@ -42,8 +42,8 @@ def applicant_form():
 def submit_applicant():
     form = MyForm(request.form, csrf_enabled=False)
     if form.validate():
-        Applicant.create(first_name=form.first_name, last_name=form.last_name,
-                         city=form.city, email=form.email, status='new')
+        Applicant.create(first_name=form.first_name.data, last_name=form.last_name.data,
+                         city=form.city.data, email=form.email.data, status='new')
         return redirect(url_for('home'))
     return redirect(url_for('applicant_form'))
 
