8d51d5e4bf4ebeef3f3ac57e8e65e215fc9f85e4
balag3 <carbalage@gmail.com>
2016-08-30 11:19:38 +0200

    Adds form class.

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect, g
+from wtforms import *
 
 app = Flask(__name__)
+
+
+
+class MyForm(Form):
+    first_name = TextField('Firs Name')
+    last_name = TextField('Last Name')
+    city = TextField('City')
+    email = EmailField('Email')
+    submit = SubmitField()
 
 
 @app.route('/')
