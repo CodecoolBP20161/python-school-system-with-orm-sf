b0f7d57fc2cabe621b891e643414c70cb9af8210
balag3 <carbalage@gmail.com>
2016-08-30 11:37:45 +0200

    Adds connection to database.

--- server.py
+++ server.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request, url_for, redirect, g
 from wtforms import *
+from connect import db
+from models import *
 
 app = Flask(__name__)
 
 
-
 class MyForm(Form):
-    first_name = TextField('Firs Name')
-    last_name = TextField('Last Name')
-    city = TextField('City')
-    email = EmailField('Email')
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    city = StringField('City')
+    email = StringField('Email')
     submit = SubmitField()
+
+
+@app.before_request
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
 
 
 @app.route('/')
 def main():
-    return 'YEAHH!!!!'
+    return "HOME"
 
 
 if __name__ == '__main__':
