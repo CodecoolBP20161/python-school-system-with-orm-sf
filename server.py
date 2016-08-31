b821ff5b0b212eb69b45efa81f7cdd13f212a8de
balag3 <carbalage@gmail.com>
2016-08-31 14:12:41 +0200

    minor fix

--- server.py
+++ server.py
@@ -14,19 +14,19 @@ app.config.update(dict(
 
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/admin/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
+        if request.form['Username'] != app.config['USERNAME']:
             error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
+        elif request.form['Password'] != app.config['PASSWORD']:
             error = 'Invalid password'
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+            return redirect(url_for('admin'))
+    return render_template('admin_login.html', error=error)
 
 
 
