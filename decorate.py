183b67a68be43090149c7695b1dd27e3503dacc2
balag3 <carbalage@gmail.com>
2016-08-31 10:01:05 +0200

    Adds email logger function.

---
+++ decorate.py
@@ -0,0 +1,28 @@
+from models import EmailLog, Mentor, Applicant
+import datetime
+
+modes = {'mentor': 'Interview details', 'applicant Interview': 'Interview time', 'applicant Details': 'Application details'}
+
+def logger(func):
+    def inner(*args, **kwargs):
+        subject = args[3]
+        content = args[1]
+        mode = [i for i in modes if modes[i] == subject][0]
+        recipient_email = args[2]
+        if mode == 'mentor':
+            mentor = Mentor.get().where(Mentor.email == recipient_email)
+            recipient_name = mentor.first_name + " " + mentor.last_name
+        else:
+            applicant = Applicant.select().where(Applicant.email == recipient_email)
+            recipient_name = applicant.first_name + " " + mentor.last_name
+
+        timestamp = datetime.datetime.now()
+
+        ret = func(*args, **kwargs)
+
+        if ret is None:
+            EmailLog.create(subject=subject,content=content,mode=mode,timestamp=timestamp,recipient_name=recipient_name,recipient_email=recipient_email,status=True)
+        else:
+            EmailLog.create(subject=subject,content=content,mode=mode,timestamp=timestamp,recipient_name=recipient_name,recipient_email=recipient_email,status=True)
+
+    return inner
